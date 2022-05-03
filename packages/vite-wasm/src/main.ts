8cccc51b60b7beac2c7e983e0d2b043ffcef9434
Eduardo Pereira de Sousa <edupsousa@gmail.com>
2022-05-03 11:36:36 -0300

    load wasm module from main.ts

--- packages/vite-wasm/src/main.ts
+++ packages/vite-wasm/src/main.ts
@@ -1,8 +1,17 @@
-import './style.css'
+import "./style.css";
+import "./wasm/dist/wasm_exec.js";
+import main from "./wasm/dist/main.wasm";
 
-const app = document.querySelector<HTMLDivElement>('#app')!
+const go = new Go();
+main(go.importObject).then((exports) => {
+  console.info(`WASM Module Loaded via Vite`);
+  const instance: WebAssembly.Instance = { exports };
+  go.run(instance);
+});
+
+const app = document.querySelector<HTMLDivElement>("#app")!;
 
 app.innerHTML = `
   <h1>Hello Vite!</h1>
   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+`;
